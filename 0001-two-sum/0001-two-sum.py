5a107f5c84f83b54de2bbf57951c45a7eef1e36f
PennyLP021 <131938742+PennyLP021@users.noreply.github.com>
2023-06-24 18:14:20 +1000

    Time: 72 ms (82.11%), Space: 17.6 MB (33.66%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -7,7 +7,7 @@ class Solution:
             if diff in prevMap:
                 return [prevMap[diff], i]
             prevMap[n] = i
-        return
+        
     
 
 # better solution using HashMap:
